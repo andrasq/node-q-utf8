4ef3bc939bc3bbcb780bb8d9559b5cbec700b1d8
Andras <andras@andrasq.com>
2019-02-23 14:19:39 -0500

    pass in to bytesToBase64 the charset to use

--- base64.js
+++ base64.js
@@ -1,7 +1,7 @@
 /**
  * base64 encode bytes, from qbson object-id.js
  *
- * Copyright (C) 2016 Andras Radics
+ * Copyright (C) 2016-2019 Andras Radics
  * Licensed under the Apache License, Version 2.0
  *
  * 2016-10-18 - AR.
@@ -28,7 +28,7 @@ module.exports = {
     encode: function base64_encode( bytes, base, bound ) {
         if (!bound || bound > bytes.length) bound = bytes.length;
         if (!base || base < 0) base = 0;
-        var str = module.exports.bytesToBase64(bytes, base, bound);
+        var str = module.exports.bytesToBase64(bytes, base, bound, _base64digits);
         if (str.length & 0x03) str += _base64pad[str.length & 0x03];
         return str;
     },
@@ -40,7 +40,7 @@ module.exports = {
  * extract the byte range from the buffer as a base64 string
  * for 12 bytes 10% slower than buf.toString, is faster for fewer
  */
-function bytesToBase64( bytes, base, bound ) {
+function bytesToBase64( bytes, base, bound, digits ) {
     var str = "";
     for (var i=base; i<bound-3; i+=3) {
         str += _emit3base64(bytes[i], bytes[i+1], bytes[i+2]);
